1c7bb1ebbdbb676dda8b3f11c37a47ca1e42ee7f
Vivero <charlie@devivero.us>
2016-05-28 12:33:29 -0400

    initial commit

---
+++ Kerbal-VR/Kerbal-VR/KerbalVrPlugin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Valve.VR;
+
+namespace KerbalVR
+{
+    // Start plugin on entering the Flight scene
+    //
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class KerbalVrPlugin : MonoBehaviour
+    {
+        /// <summary>
+        ///  Overrides the Start method for a MonoBehaviour plugin.
+        /// </summary>
+        void Start()
+        {
+            Debug.Log("[Kerbal-VR] KerbalVrPlugin started.");
+        }
+
+        /// <summary>
+        ///  Overrides the Update method, called every frame.
+        /// </summary>
+        void Update()
+        {
+            if (CameraManager.Instance.currentCameraMode != CameraManager.CameraMode.IVA)
+            {
+                return;
+            }
+        }
+    }
+
+}
