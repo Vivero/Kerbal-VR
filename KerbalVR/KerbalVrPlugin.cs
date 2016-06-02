7e2dcef4f1ef612f38d0d16ee43960a82719bff7
Vivero <charlie@devivero.us>
2016-06-01 23:00:50 -0400

    update build versioning information and build events

--- KerbalVR/KerbalVrPlugin.cs
+++ KerbalVR/KerbalVrPlugin.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+//using System.Runtime.InteropServices;
+//using System.Text;
+//using System.Threading.Tasks;
 using UnityEngine;
 using Valve.VR;
 
