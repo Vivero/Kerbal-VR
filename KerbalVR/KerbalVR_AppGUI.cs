7850c87495aa504c8dad61fe19c64b742c69a954
Vivero <devivero.c@gmail.com>
2018-03-13 22:27:03 -0700

    fix issues with GUI not opening/closing correctly throughout scene changes

--- KerbalVR/KerbalVR_AppGUI.cs
+++ KerbalVR/KerbalVR_AppGUI.cs
@@ -27,6 +27,7 @@ namespace KerbalVR
 
         private ApplicationLauncherButton appButton;
         private bool appButtonGuiActive = false;
+        private bool appButtonGuiActiveLastState = false;
 
         private Rect appGuiWindowRect = new Rect(Screen.width / 4, Screen.height / 4, 160, 100);
 
@@ -38,10 +39,14 @@ namespace KerbalVR
         /// <summary>
         /// This GameEvent is registered with GameEvents.onGUIApplicationLauncherReady,
         /// at the time the plugin is loaded. It instantiates a new button on the
-        /// application launcher.
+        /// application launcher. Note that this callback can be called multiple times
+        /// throughout the game.
         /// </summary>
         public void OnAppLauncherReady() {
             // define where should the app button be visible
+#if DEBUG
+            ApplicationLauncher.AppScenes appVisibility = ApplicationLauncher.AppScenes.ALWAYS;
+#else
             ApplicationLauncher.AppScenes appVisibility =
                 ApplicationLauncher.AppScenes.SPACECENTER |
                 ApplicationLauncher.AppScenes.FLIGHT |
@@ -49,19 +54,32 @@ namespace KerbalVR
                 ApplicationLauncher.AppScenes.VAB |
                 ApplicationLauncher.AppScenes.SPH |
                 ApplicationLauncher.AppScenes.TRACKSTATION;
-            
-            // create new app button instance
-            if (HighLogic.LoadedScene == GameScenes.SPACECENTER &&
-                appButton == null) {
+#endif
+
+            // create new app button instance if it doesn't already exist
+            if (appButton == null) {
                 appButton = ApplicationLauncher.Instance.AddModApplication(
                     OnToggleTrue,
                     OnToggleFalse,
                     null, null, null, null,
                     appVisibility,
                     GameDatabase.Instance.GetTexture(AppButtonLogo, false));
-            }
 
-            // GUI is off at instantiation
+                // GUI is off at instantiation
+                appButtonGuiActive = false;
+
+                // register callbacks when AppLauncher shows/hides (i.e. during loading screens)
+                ApplicationLauncher.Instance.AddOnShowCallback(OnShow);
+                ApplicationLauncher.Instance.AddOnHideCallback(OnHide);
+            }
+        }
+
+        void OnShow() {
+            appButtonGuiActive = appButtonGuiActiveLastState;
+        }
+
+        void OnHide() {
+            appButtonGuiActiveLastState = appButtonGuiActive;
             appButtonGuiActive = false;
         }
 
@@ -72,7 +90,10 @@ namespace KerbalVR
         /// </summary>
         public void OnAppLauncherDestroyed() {
             if (appButton != null) {
+                OnToggleFalse();
                 ApplicationLauncher.Instance.RemoveApplication(appButton);
+                ApplicationLauncher.Instance.RemoveOnShowCallback(OnShow);
+                ApplicationLauncher.Instance.RemoveOnHideCallback(OnHide);
             }
         }
 
