38c1e791581d18ed2360eb53d6bb3bac6c180d58
Vivero <devivero.c@gmail.com>
2018-03-15 15:35:18 -0700

    experiment with InternalModule

---
+++ KerbalVR/Modules/KVR_ToggleSwitch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KerbalVR.Modules
+{
+    public class KVR_ToggleSwitch : InternalModule
+    {
+        void OnStart() {
+            Utils.LogInfo("InternalModule Start " + gameObject.name);
+        }
+    }
+}
