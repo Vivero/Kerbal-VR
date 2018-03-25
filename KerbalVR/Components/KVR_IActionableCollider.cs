89af0b9d635b3b18ac8dabcff3d8ac321b7b1532
Vivero <devivero.c@gmail.com>
2018-03-25 12:50:23 -0700

    control stick button de-bounce; refactor stick collider detection

--- KerbalVR/Components/KVR_IActionableCollider.cs
+++ KerbalVR/Components/KVR_IActionableCollider.cs
@@ -5,7 +5,7 @@ namespace KerbalVR.Components
     public interface IActionableCollider
     {
         void OnColliderEntered(Collider thisObject, Collider otherObject);
-        void OnColliderStayed(Collider thisObject, Collider otherObject);
+        // void OnColliderStayed(Collider thisObject, Collider otherObject);
         void OnColliderExited(Collider thisObject, Collider otherObject);
     }
 }
