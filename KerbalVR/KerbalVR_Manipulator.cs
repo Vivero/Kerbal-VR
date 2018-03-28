99612a17d954f49df1a1db1ed8de07b801526592
Vivero <devivero.c@gmail.com>
2018-03-27 21:03:19 -0700

    refactor manipulator

--- KerbalVR/KerbalVR_Manipulator.cs
+++ KerbalVR/KerbalVR_Manipulator.cs
@@ -6,19 +6,27 @@ namespace KerbalVR
 {
     public class Manipulator : MonoBehaviour
     {
+        #region Properties
         public SteamVR_Controller.Device State { get; private set; }
+        #endregion
+
+        #region Members
+        public ETrackedControllerRole role;
+        public Color defaultColor = Color.white;
+        public Color activeColor = Color.black;
+        #endregion
+
+        #region Private Members
+        private MeshRenderer meshRenderer;
+        private int numCollidersTouching = 0;
+        #endregion
+
 
         // TODO: think about these fields
-        public Color originalColor = Color.white;
-        public Color activeColor = Color.black;
         public float movementVelocity = 1f;
 
-        public ETrackedControllerRole role;
 
-        private MeshRenderer meshRenderer;
-
-        void Awake() {
-            // Utils.Log("Manipulator alive.");
+        void Start() {
             meshRenderer = GetComponent<MeshRenderer>();
         }
 
@@ -28,6 +36,11 @@ namespace KerbalVR
 #endif
         }
 
+        /// <summary>
+        /// Stores the latest transform and button state information.
+        /// </summary>
+        /// <param name="pose">Updated pose data</param>
+        /// <param name="state">Updated state data</param>
         public void UpdateState(SteamVR_Utils.RigidTransform pose, SteamVR_Controller.Device state) {
             State = state;
 
@@ -81,22 +94,19 @@ namespace KerbalVR
             }
         }
 
-        void OnDestroy() {
-            // Utils.Log("Manipulator " + role + " is being destroyed!");
-        }
-
-        void OnTriggerStay(Collider other) {
-            // Utils.Log("OnTriggerEnter! " + other.name);
-
-            MeshRenderer r = gameObject.GetComponent<MeshRenderer>();
-            r.sharedMaterial.color = activeColor;
+        void OnTriggerEnter(Collider other) {
+            // keep count of how many other colliders we've entered
+            numCollidersTouching += 1;
+            meshRenderer.sharedMaterial.color = activeColor;
         }
 
         void OnTriggerExit(Collider other) {
-            // Utils.Log("Manipulator OnTriggerExit " + other.name);
-
-            MeshRenderer r = gameObject.GetComponent<MeshRenderer>();
-            r.sharedMaterial.color = originalColor;
+            // when number of colliders exited drops back down to zero, reset default color
+            numCollidersTouching -= 1;
+            if (numCollidersTouching <= 0) {
+                numCollidersTouching = 0;
+                meshRenderer.sharedMaterial.color = defaultColor;
+            }
         }
     }
 }
