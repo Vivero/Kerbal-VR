0fddcfac917b50b6eb72095830d4630b02a25012
Vivero <devivero.c@gmail.com>
2018-04-01 16:26:44 -0700

    factor out labels and button cover code

---
+++ KerbalVR/Components/KVR_Cover.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using UnityEngine;
+
+namespace KerbalVR.Components
+{
+    public class KVR_Cover : IActionableCollider
+    {
+        #region Types
+        public enum State {
+            Closed,
+            Open,
+        }
+
+        public enum StateInput {
+            ColliderEnter,
+            FinishedAction,
+        }
+
+        public enum FSMState {
+            IsClosed,
+            IsOpening,
+            IsOpen,
+            IsClosing,
+        }
+        #endregion
+
+        #region Properties
+        public State CurrentCoverState { get; private set; }
+        public Animation CoverAnimation { get; private set; }
+        public Transform ColliderTransform { get; private set; }
+        #endregion
+
+        #region Private Members
+        private string coverAnimationName;
+        private AnimationState coverAnimationState;
+        private GameObject colliderGameObject;
+        private FSMState fsmState;
+        private float targetAnimationEndTime;
+        private bool isAnimationPlayingPrev = false;
+        #endregion
+
+        #region Constructors
+        public KVR_Cover(InternalProp prop, ConfigNode configuration) {
+            // animation
+            bool success = configuration.TryGetValue("animationName", ref coverAnimationName);
+            if (success) {
+                Animation[] animations = prop.FindModelAnimators(coverAnimationName);
+                if (animations.Length > 0) {
+                    CoverAnimation = animations[0];
+                    coverAnimationState = CoverAnimation[coverAnimationName];
+                    coverAnimationState.wrapMode = WrapMode.Once;
+                } else {
+                    throw new ArgumentException("InternalProp \"" + prop.name + "\" does not have animations (config node " +
+                        configuration.id + ")");
+                }
+            } else {
+                throw new ArgumentException("animationName not specified for KVR_Cover " +
+                    prop.name + " (config node " + configuration.id + ")");
+            }
+
+            // collider game object
+            string colliderTransformName = "";
+            success = configuration.TryGetValue("colliderTransformName", ref colliderTransformName);
+            if (!success) throw new ArgumentException("colliderTransformName not specified for KVR_Cover " +
+                prop.name + " (config node " + configuration.id + ")");
+
+            ColliderTransform = prop.FindModelTransform(colliderTransformName);
+            if (ColliderTransform == null) throw new ArgumentException("Transform \"" + colliderTransformName +
+                "\" not found for KVR_Cover " + prop.name + " (config node " + configuration.id + ")");
+
+            colliderGameObject = ColliderTransform.gameObject;
+            colliderGameObject.AddComponent<KVR_ActionableCollider>();
+
+            // set initial state
+            isAnimationPlayingPrev = false;
+            fsmState = FSMState.IsClosed;
+            targetAnimationEndTime = 0f;
+            GoToState(State.Closed);
+        }
+        #endregion
+
+        public void Update() {
+            bool isAnimationPlaying = CoverAnimation.isPlaying;
+
+            // check if animation finished playing
+            if (!isAnimationPlaying && isAnimationPlayingPrev) {
+                UpdateFSM(StateInput.FinishedAction);
+            }
+
+            // keep track of whether animation was playing
+            isAnimationPlayingPrev = isAnimationPlaying;
+        }
+
+        private void UpdateFSM(StateInput input) {
+            switch (fsmState) {
+                case FSMState.IsClosed:
+                    if (input == StateInput.ColliderEnter) {
+                        fsmState = FSMState.IsOpening;
+                        PlayToState(State.Open);
+                    }
+                    break;
+
+                case FSMState.IsOpening:
+                    if (input == StateInput.FinishedAction) {
+                        fsmState = FSMState.IsOpen;
+                    }
+                    break;
+
+                case FSMState.IsOpen:
+                    if (input == StateInput.ColliderEnter) {
+                        fsmState = FSMState.IsClosing;
+                        PlayToState(State.Closed);
+                    }
+                    break;
+
+                case FSMState.IsClosing:
+                    if (input == StateInput.FinishedAction) {
+                        fsmState = FSMState.IsClosed;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public void OnColliderEntered(Collider thisObject, Collider otherObject) {
+            if (DeviceManager.IsManipulator(otherObject.gameObject)) {
+
+                if (thisObject.gameObject == colliderGameObject) {
+                    // when cover is closed, can only be opened from the bottom edge of the
+                    // collider on the top side. when cover is open, can only be opened from the top
+                    // side of the collider.
+                    Vector3 manipulatorDeltaPos = colliderGameObject.transform.InverseTransformPoint(
+                        otherObject.transform.position);
+                    if ((CurrentCoverState == State.Closed &&
+                        manipulatorDeltaPos.z > 0f &&
+                        manipulatorDeltaPos.y > 0f) ||
+                        (CurrentCoverState == State.Open &&
+                        manipulatorDeltaPos.y > 0f))
+
+                        UpdateFSM(StateInput.ColliderEnter);
+                }
+            }
+        }
+
+        public void OnColliderExited(Collider thisObject, Collider otherObject) { }
+
+        public void SetState(State state) {
+            CurrentCoverState = state;
+        }
+
+        private void GoToState(State state) {
+            SetState(state);
+
+            // switch to animation state instantly
+            coverAnimationState.normalizedTime = GetNormalizedTimeForState(state);
+            coverAnimationState.speed = 0f;
+            CoverAnimation.Play(coverAnimationName);
+        }
+
+        private void PlayToState(State state) {
+
+            // set the animation time that we want to play to
+            targetAnimationEndTime = GetNormalizedTimeForState(state);
+
+            // note that the normalizedTime always resets to zero after finishing the clip.
+            // so if cover was at Open and it was already done playing, its normalizedTime is
+            // 0f, even though the Open state corresponds to a time of 1f. so, for this special
+            // case, force it to 1f.
+            if (CurrentCoverState == State.Open &&
+                coverAnimationState.normalizedTime == 0f &&
+                !CoverAnimation.isPlaying) {
+                coverAnimationState.normalizedTime = 1f;
+            }
+
+            // move either up or down depending on where the switch is right now
+            coverAnimationState.speed =
+                Mathf.Sign(targetAnimationEndTime - coverAnimationState.normalizedTime) * 1f;
+
+            // play animation and actuate switch
+            CoverAnimation.Play(coverAnimationName);
+            SetState(state);
+        }
+
+        private float GetNormalizedTimeForState(State state) {
+            float targetTime = 0f;
+            switch (state) {
+                case State.Closed:
+                    targetTime = 0f;
+                    break;
+                case State.Open:
+                    targetTime = 1f;
+                    break;
+            }
+            return targetTime;
+        }
+    }
+}
--- KerbalVR/Modules/KVR_PushButton.cs
+++ KerbalVR/Modules/KVR_PushButton.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using KerbalVR.Components;
 
@@ -8,25 +9,6 @@ namespace KerbalVR.Modules
     {
 
         #region Types
-        public enum CoverState {
-            Closed,
-            Open,
-        }
-
-        public enum CoverStateInput
-        {
-            ColliderEnter,
-            FinishedAction,
-        }
-
-        public enum CoverFSMState
-        {
-            IsClosed,
-            IsOpening,
-            IsOpen,
-            IsClosing,
-        }
-
         public enum ButtonState {
             Unpressed,
             Pressed,
@@ -50,10 +32,6 @@ namespace KerbalVR.Modules
 
         #region KSP Config Fields
         [KSPField]
-        public string coverAnimationName = string.Empty;
-        [KSPField]
-        public string transformCoverCollider = string.Empty;
-        [KSPField]
         public string buttonAnimationName = string.Empty;
         [KSPField]
         public string transformButtonCollider = string.Empty;
@@ -62,19 +40,14 @@ namespace KerbalVR.Modules
         #endregion
 
         #region Properties
-        public CoverState CurrentCoverState { get; private set; }
         public ButtonState CurrentButtonState { get; private set; }
+        public List<KVR_Label> Labels { get; private set; } = new List<KVR_Label>();
         #endregion
 
         #region Private Members
         private ConfigNode moduleConfigNode;
 
-        private Animation coverAnimation;
-        private AnimationState coverAnimationState;
-        private GameObject coverGameObject;
-        private CoverFSMState coverFSMState;
-        private float targetCoverAnimationEndTime;
-        private bool isCoverAnimationPlayingPrev;
+        private KVR_Cover buttonCover;
 
         private Animation buttonAnimation;
         private AnimationState buttonAnimationState;
@@ -86,23 +59,27 @@ namespace KerbalVR.Modules
         private GameObject coloredGameObject;
         private Material coloredGameObjectMaterial;
         #endregion
+        
 
+        protected void Start() {
+            Utils.Log("KVR_PushButton Start");
 
-        void Start() {
             // no setup needed in editor mode
             if (HighLogic.LoadedScene == GameScenes.EDITOR) return;
 
-            // retrieve the animations
-            Animation[] animations = internalProp.FindModelAnimators(coverAnimationName);
-            if (animations.Length > 0) {
-                coverAnimation = animations[0];
-                coverAnimationState = coverAnimation[coverAnimationName];
-                coverAnimationState.wrapMode = WrapMode.Once;
-            } else {
-                Utils.LogWarning("KVR_PushButton (" + gameObject.name + ") has no animation \"" + coverAnimationName + "\"");
+            // obtain module configuration
+            moduleConfigNode = ConfigUtils.GetModuleConfigNode(internalProp.name, moduleID);
+
+            // if there's a cover, create it
+            ConfigNode coverConfigNode = moduleConfigNode.GetNode("KVR_COVER");
+            try {
+                buttonCover = new KVR_Cover(internalProp, coverConfigNode);
+            } catch (Exception e) {
+                Utils.LogWarning("KVR_PushButton: cover exception: " + e.ToString());
             }
 
-            animations = internalProp.FindModelAnimators(buttonAnimationName);
+            // retrieve the animations
+            Animation[] animations = internalProp.FindModelAnimators(buttonAnimationName);
             if (animations.Length > 0) {
                 buttonAnimation = animations[0];
                 buttonAnimationState = buttonAnimation[buttonAnimationName];
@@ -112,15 +89,7 @@ namespace KerbalVR.Modules
             }
 
             // retrieve the collider GameObjects
-            Transform colliderTransform = internalProp.FindModelTransform(transformCoverCollider);
-            if (colliderTransform != null) {
-                coverGameObject = colliderTransform.gameObject;
-                coverGameObject.AddComponent<KVR_ActionableCollider>().module = this;
-            } else {
-                Utils.LogWarning("KVR_PushButton (" + gameObject.name + ") has no cover collider \"" + transformCoverCollider + "\"");
-            }
-
-            colliderTransform = internalProp.FindModelTransform(transformButtonCollider);
+            Transform colliderTransform = internalProp.FindModelTransform(transformButtonCollider);
             if (colliderTransform != null) {
                 buttonGameObject = colliderTransform.gameObject;
                 buttonGameObject.AddComponent<KVR_ActionableCollider>().module = this;
@@ -137,11 +106,6 @@ namespace KerbalVR.Modules
             }
 
             // set initial state
-            isCoverAnimationPlayingPrev = false;
-            coverFSMState = CoverFSMState.IsClosed;
-            targetCoverAnimationEndTime = 0f;
-            GoToCoverState(CoverState.Closed);
-
             isButtonAnimationPlayingPrev = false;
             buttonFSMState = ButtonFSMState.IsUnpressed;
             targetButtonAnimationEndTime = 0f;
@@ -152,51 +116,17 @@ namespace KerbalVR.Modules
         }
 
         public override void OnUpdate() {
-            bool isCoverAnimationPlaying = coverAnimation.isPlaying;
-            bool isButtonAnimationPlaying = buttonAnimation.isPlaying;
-
-            if (!isCoverAnimationPlaying && isCoverAnimationPlayingPrev) {
-                UpdateCoverFSM(CoverStateInput.FinishedAction);
+            // update the button cover if available
+            if (buttonCover != null) {
+                buttonCover.Update();
             }
+            
+            bool isButtonAnimationPlaying = buttonAnimation.isPlaying;
             if (!isButtonAnimationPlaying && isButtonAnimationPlayingPrev) {
                 UpdateButtonFSM(ButtonStateInput.FinishedAction);
             }
-
-            isCoverAnimationPlayingPrev = isCoverAnimationPlaying;
+            
             isButtonAnimationPlayingPrev = isButtonAnimationPlaying;
-        }
-
-        private void UpdateCoverFSM(CoverStateInput input) {
-            switch (coverFSMState) {
-                case CoverFSMState.IsClosed:
-                    if (input == CoverStateInput.ColliderEnter) {
-                        coverFSMState = CoverFSMState.IsOpening;
-                        PlayToCoverState(CoverState.Open);
-                    }
-                    break;
-
-                case CoverFSMState.IsOpening:
-                    if (input == CoverStateInput.FinishedAction) {
-                        coverFSMState = CoverFSMState.IsOpen;
-                    }
-                    break;
-
-                case CoverFSMState.IsOpen:
-                    if (input == CoverStateInput.ColliderEnter) {
-                        coverFSMState = CoverFSMState.IsClosing;
-                        PlayToCoverState(CoverState.Closed);
-                    }
-                    break;
-
-                case CoverFSMState.IsClosing:
-                    if (input == CoverStateInput.FinishedAction) {
-                        coverFSMState = CoverFSMState.IsClosed;
-                    }
-                    break;
-
-                default:
-                    break;
-            }
         }
 
         private void UpdateButtonFSM(ButtonStateInput input) {
@@ -235,22 +165,8 @@ namespace KerbalVR.Modules
         public void OnColliderEntered(Collider thisObject, Collider otherObject) {
             if (DeviceManager.IsManipulator(otherObject.gameObject)) {
 
-                if (thisObject.gameObject == coverGameObject) {
-                    // when cover is closed, can only be opened from the
-                    // bottom edge of the collider on the top side.
-                    // when cover is open, can only be opened from the top
-                    // side of the collider.
-                    Vector3 manipulatorDeltaPos = coverGameObject.transform.InverseTransformPoint(
-                        otherObject.transform.position);
-                    if ((CurrentCoverState == CoverState.Closed &&
-                        manipulatorDeltaPos.z > 0f &&
-                        manipulatorDeltaPos.y > 0f) ||
-                        (CurrentCoverState == CoverState.Open &&
-                        manipulatorDeltaPos.y > 0f))
-                        UpdateCoverFSM(CoverStateInput.ColliderEnter);
-
-                } else if (CurrentCoverState == CoverState.Open &&
-                    thisObject.gameObject == buttonGameObject) {
+                if (thisObject.gameObject == buttonGameObject && ((buttonCover == null) ||
+                    (buttonCover != null && buttonCover.CurrentCoverState == KVR_Cover.State.Open))) {
                     // button is pressed while cover is OPEN, and collider
                     // has entered from the top side of the button
                     Vector3 manipulatorDeltaPos = buttonGameObject.transform.InverseTransformPoint(
@@ -264,43 +180,6 @@ namespace KerbalVR.Modules
         }
 
         public void OnColliderExited(Collider thisObject, Collider otherObject) { }
-
-        public void SetCoverState(CoverState state) {
-            CurrentCoverState = state;
-        }
-
-        private void GoToCoverState(CoverState state) {
-            SetCoverState(state);
-
-            // switch to animation state instantly
-            coverAnimationState.normalizedTime = GetNormalizedTimeForCoverState(state);
-            coverAnimationState.speed = 0f;
-            coverAnimation.Play(coverAnimationName);
-        }
-
-        private void PlayToCoverState(CoverState state) {
-
-            // set the animation time that we want to play to
-            targetCoverAnimationEndTime = GetNormalizedTimeForCoverState(state);
-
-            // note that the normalizedTime always resets to zero after finishing the clip.
-            // so if cover was at Open and it was already done playing, its normalizedTime is
-            // 0f, even though the Open state corresponds to a time of 1f. so, for this special
-            // case, force it to 1f.
-            if (CurrentCoverState == CoverState.Open &&
-                coverAnimationState.normalizedTime == 0f &&
-                !coverAnimation.isPlaying) {
-                coverAnimationState.normalizedTime = 1f;
-            }
-
-            // move either up or down depending on where the switch is right now
-            coverAnimationState.speed =
-                Mathf.Sign(targetCoverAnimationEndTime - coverAnimationState.normalizedTime) * 1f;
-
-            // play animation and actuate switch
-            coverAnimation.Play(coverAnimationName);
-            SetCoverState(state);
-        }
 
         public void SetButtonState(ButtonState state) {
             CurrentButtonState = state;
@@ -345,19 +224,6 @@ namespace KerbalVR.Modules
             SetButtonState(state);
         }
 
-        private float GetNormalizedTimeForCoverState(CoverState state) {
-            float targetTime = 0f;
-            switch (state) {
-                case CoverState.Open:
-                    targetTime = 1f;
-                    break;
-                case CoverState.Closed:
-                    targetTime = 0f;
-                    break;
-            }
-            return targetTime;
-        }
-
         private float GetNormalizedTimeForButtonState(ButtonState state) {
             float targetTime = 0f;
             switch (state) {
@@ -376,93 +242,15 @@ namespace KerbalVR.Modules
             moduleConfigNode = ConfigUtils.GetModuleConfigNode(internalProp.name, moduleID);
             ConfigNode[] labelNodes = moduleConfigNode.GetNodes("KVR_LABEL");
             for (int i = 0; i < labelNodes.Length; i++) {
-                CreateLabelFromConfig(labelNodes[i], i);
+                ConfigNode node = labelNodes[i];
+                node.id = i.ToString();
+                try {
+                    KVR_Label label = new KVR_Label(internalProp, node);
+                    Labels.Add(label);
+                } catch (Exception e) {
+                    Utils.LogWarning(e.ToString());
+                }
             }
-        }
-
-        private void CreateLabelFromConfig(ConfigNode cfgLabelNode, int id = 0) {
-            // label text
-            string labelText = "";
-            bool success = cfgLabelNode.TryGetValue("text", ref labelText);
-
-            // label transform (where to place the label)
-            string cfgLabelTransform = "";
-            success = cfgLabelNode.TryGetValue("transform", ref cfgLabelTransform);
-            if (!success) throw new ArgumentException("Transform not specified for label " + labelText + " (" + cfgLabelNode.id + ")");
-
-            Transform labelTransform = internalProp.FindModelTransform(cfgLabelTransform);
-            if (labelTransform == null) throw new ArgumentException("Transform not found for label " + labelText + " (prop: " + internalProp.name + ")");
-
-            // position offset from the transform
-            Vector3 labelPositionOffset = Vector3.zero;
-            success = cfgLabelNode.TryGetValue("transformPositionOffset", ref labelPositionOffset);
-
-            // rotation offset from the transform
-            Quaternion labelRotationOffset = Quaternion.identity;
-            success = cfgLabelNode.TryGetValue("transformRotationOffset", ref labelRotationOffset);
-
-            // font size
-            float labelFontSize = 0.14f;
-            success = cfgLabelNode.TryGetValue("fontSize", ref labelFontSize);
-
-            // font style (bold, italics, etc)
-            TMPro.FontStyles labelFontStyle = TMPro.FontStyles.Normal;
-            success = cfgLabelNode.TryGetEnum("fontStyle", ref labelFontStyle, TMPro.FontStyles.Normal);
-
-            // font alignment
-            TMPro.TextAlignmentOptions labelAlignment = TMPro.TextAlignmentOptions.Center;
-            success = cfgLabelNode.TryGetEnum("alignment", ref labelAlignment, TMPro.TextAlignmentOptions.Center);
-
-            // canvas pivot (anchor point on the canvas)
-            Vector2 labelPivot = new Vector2(0.5f, 0.5f);
-            success = cfgLabelNode.TryGetValue("pivot", ref labelPivot);
-
-            // size of the label canvas
-            Vector2 labelSize = new Vector2(0.2f, 0.2f);
-            success = cfgLabelNode.TryGetValue("canvasSize", ref labelSize);
-
-            CreateLabel(id, labelText, labelFontSize, labelFontStyle,
-                labelAlignment, labelTransform, labelPositionOffset,
-                labelRotationOffset, labelPivot, labelSize);
-        }
-
-        private GameObject CreateLabel(
-            int id,
-            string text,
-            float fontSize,
-            TMPro.FontStyles fontStyle,
-            TMPro.TextAlignmentOptions alignment,
-            Transform labelTransform,
-            Vector3 labelPositionOffset,
-            Quaternion labelRotationOffset,
-            Vector2 rectPivot,
-            Vector2 rectSize) {
-
-            string labelName = internalProp.name + "-" + internalProp.propID + "-" + id;
-            // Utils.Log("Creating KVR_LABEL: \"" + labelName + "\"");
-
-            GameObject labelGameObject = new GameObject(labelName);
-            labelGameObject.layer = 20;
-            labelGameObject.transform.SetParent(labelTransform);
-
-            TMPro.TextMeshPro tmpLabel = labelGameObject.AddComponent<TMPro.TextMeshPro>();
-            tmpLabel.SetText(text);
-            tmpLabel.fontSize = fontSize;
-            tmpLabel.fontStyle = fontStyle;
-            tmpLabel.alignment = alignment;
-
-            tmpLabel.rectTransform.pivot = rectPivot;
-            tmpLabel.rectTransform.localPosition = labelPositionOffset;
-            tmpLabel.rectTransform.localRotation = labelRotationOffset;
-            tmpLabel.rectTransform.sizeDelta = rectSize;
-
-            // find and set font
-            TMPro.TMP_FontAsset font = Resources.Load("Font/LiberationSans SDF", typeof(TMPro.TMP_FontAsset)) as TMPro.TMP_FontAsset;
-            if (font != null) {
-                tmpLabel.font = font;
-            }
-
-            return labelGameObject;
         }
     }
 }
