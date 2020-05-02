23b5e661abeb250bf80b18b903d9352a8c2e221e
Vivero <devivero.c@gmail.com>
2020-05-02 02:33:21 -0700

    update bindings

--- KerbalVR_UnitySteamVR/export_input_bindings.ps1
+++ KerbalVR_UnitySteamVR/export_input_bindings.ps1
@@ -1,6 +1,8 @@
-$DocumentsPath = [System.Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments)
-Copy-Item -ErrorAction SilentlyContinue "$DocumentsPath\steamvr\input\application.generated.unity.kerbalvr_unitysteamvr.exe_knuckles.json" ..\KerbalVR_Mod\KerbalVR\Assets\Input\bindings_knuckles.json
-Copy-Item -ErrorAction SilentlyContinue "$DocumentsPath\steamvr\input\application.generated.unity.kerbalvr_unitysteamvr.exe_oculus_touch.json" ..\KerbalVR_Mod\KerbalVR\Assets\Input\bindings_oculus_touch.json
-Copy-Item -ErrorAction SilentlyContinue "$DocumentsPath\steamvr\input\application.generated.unity.kerbalvr_unitysteamvr.exe_vive_controller.json" ..\KerbalVR_Mod\KerbalVR\Assets\Input\bindings_vive_controller.json
+# $DocumentsPath = [System.Environment]::GetFolderPath([Environment+SpecialFolder]::MyDocuments)
+# Copy-Item -ErrorAction SilentlyContinue "$DocumentsPath\steamvr\input\application.generated.unity.kerbalvr_unitysteamvr.exe_knuckles.json" ..\KerbalVR_Mod\KerbalVR\Assets\Input\bindings_knuckles.json
+# Copy-Item -ErrorAction SilentlyContinue "$DocumentsPath\steamvr\input\application.generated.unity.kerbalvr_unitysteamvr.exe_oculus_touch.json" ..\KerbalVR_Mod\KerbalVR\Assets\Input\bindings_oculus_touch.json
+# Copy-Item -ErrorAction SilentlyContinue "$DocumentsPath\steamvr\input\application.generated.unity.kerbalvr_unitysteamvr.exe_vive_controller.json" ..\KerbalVR_Mod\KerbalVR\Assets\Input\bindings_vive_controller.json
+
+Copy-Item .\Assets\StreamingAssets\SteamVR\*.json ..\KerbalVR_Mod\KerbalVR\Assets\Input\.
 Copy-Item .\Assets\SteamVR_Input\*.cs ..\KerbalVR_Mod\KerbalVR\SteamVR_Input\.
 Copy-Item .\Assets\SteamVR_Input\ActionSetClasses\*.cs ..\KerbalVR_Mod\KerbalVR\SteamVR_Input\ActionSetClasses\.
