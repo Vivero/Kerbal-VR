ff94b4eeb5beb86ec1c34b1a81639b3829e0bdfd
Vivero <devivero.c@gmail.com>
2020-05-03 13:46:13 -0700

    add simple shift register structure

--- KerbalVR_Mod/KerbalVR/KerbalVR_Types.cs
+++ KerbalVR_Mod/KerbalVR/KerbalVR_Types.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,14 +7,6 @@ namespace KerbalVR
     public class Types
     {
         // struct to keep track of Camera properties
-        public struct CameraData
-        {
-            public Camera camera;
-            public Matrix4x4 originalProjectionMatrix;
-            public Matrix4x4 hmdProjectionMatrixL;
-            public Matrix4x4 hmdProjectionMatrixR;
-        }
-
         public class VRCameraSet {
             public VREyeCamera[] vrCameras;
             public string kspCameraName;
@@ -28,6 +21,35 @@ namespace KerbalVR
 
         public class CameraState {
             public bool enabled;
+        }
+
+        /// <summary>
+        /// A very simple, basic, generic shift register structure.
+        /// Stores values, and detects when values have changed.
+        /// </summary>
+        /// <typeparam name="T">Any type that implements IEquatable</typeparam>
+        public class ShiftRegister<T> where T : IEquatable<T> {
+            public List<T> Values { get; private set; }
+            private int maxSize;
+
+            public ShiftRegister(uint registerSize) {
+                maxSize = (int)registerSize;
+                Values = new List<T>(maxSize);
+            }
+
+            public void Push(T value) {
+                Values.Insert(0, value);
+
+                // delete old items
+                for (int i = Values.Count - 1; i > (maxSize - 1); --i) {
+                    Values.RemoveAt(i);
+                }
+            }
+
+            public bool IsChanged() {
+                if (Values.Count <= 1) return false;
+                return !(Values[1].Equals(Values[0]));
+            }
         }
     }
 }
